68b5ca82a5b3161986f7ee5da9a6d72b0a0f044d
jipeng <jipeng@able-elec.com>
2019-07-11 13:55:27 +0800

    durid数据源

---
+++ jeektime/duriddatasource/src/main/java/com/able/duriddatasource/DuriddatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.able.duriddatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuriddatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuriddatasourceApplication.class, args);
+    }
+
+}
---
+++ jeektime/duriddatasource/src/test/java/com/able/duriddatasource/DuriddatasourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.able.duriddatasource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DuriddatasourceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
