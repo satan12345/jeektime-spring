1564aa793a045650e0a32b597736c591e6fcd7ab
jipeng <jipeng@able-elec.com>
2019-07-11 13:32:27 +0800

    Hikari数据源连接池

---
+++ jeektime/hikricpdatasource/src/main/java/com/able/hikricpdatasource/HikricpdatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.able.hikricpdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikricpdatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikricpdatasourceApplication.class, args);
+    }
+
+}
